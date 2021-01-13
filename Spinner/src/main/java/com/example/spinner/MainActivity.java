e6904c404b38fc392e7301ac153e5f497a6aea51
sivanliu <lygforbs0@gmail.com>
2021-01-13 21:04:12 +0800

    spinner

--- Spinner/src/main/java/com/example/spinner/MainActivity.java
+++ Spinner/src/main/java/com/example/spinner/MainActivity.java
@@ -1,32 +1,54 @@
 
 package com.example.spinner;
 
+import com.lxj.xpopup.XPopup;
+import com.lxj.xpopup.core.AttachPopupView;
+import com.lxj.xpopup.core.BasePopupView;
+import com.lxj.xpopup.enums.PopupPosition;
+import com.lxj.xpopup.enums.PopupType;
+import com.lxj.xpopup.interfaces.OnSelectListener;
+import com.lxj.xpopup.interfaces.XPopupCallback;
+
+import android.content.res.ColorStateList;
+import android.graphics.Color;
+import android.graphics.ColorSpace;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
+import android.provider.CalendarContract;
 import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 
 import java.util.ArrayList;
 import java.util.List;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
 
     private SpinnerAdapter spinnerAdapter;
     private Spinner mSpinner;
+    private RelativeLayout ll_view;
+    private RecyclerView recyclerView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         mSpinner = findViewById(R.id.spinner_choose_family);
+        ll_view = findViewById(R.id.ll_view);
+        recyclerView = findViewById(R.id.recyclerView);
+
+
 
         List<SpinnerBean> spinnerBeans = new ArrayList<>();
         SpinnerBean spinnerBean = new SpinnerBean();
         spinnerBean.setUrl("https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM40paJx3MJG6HOy0sk2LA2IH5u85EO4zurFB1ZNZgkb8pyWtCyiajaLG2ITZwNdomR1E5YMEqibJeFA/132");
-        spinnerBean.setName("刘小芸");
+        spinnerBean.setName("刘小芸刘刘小芸刘刘小芸刘");
 
         SpinnerBean spinnerBean1 = new SpinnerBean();
         spinnerBean1.setUrl("https://thirdwx.qlogo.cn/mmopen/vi_32/Q3auHgzwzM40paJx3MJG6HOy0sk2LA2IH5u85EO4zurFB1ZNZgkb8pyWtCyiajaLG2ITZwNdomR1E5YMEqibJeFA/132");
@@ -49,17 +71,92 @@ public class MainActivity extends AppCompatActivity {
         spinnerBeans.add(spinnerBean2);
         spinnerBeans.add(spinnerBean3);
         spinnerBeans.add(spinnerBean4);
-        spinnerAdapter = new SpinnerAdapter(this, spinnerBeans);
+        spinnerAdapter = new SpinnerAdapter(this, mSpinner, spinnerBeans);
         mSpinner.setAdapter(spinnerAdapter);
         mSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 Log.e("sss", "position = " + position);
+//                view.setBackgroundColor(Color.TRANSPARENT);
+                spinnerAdapter.select(position);
+
             }
 
             @Override
             public void onNothingSelected(AdapterView<?> parent) {
 
+            }
+        });
+
+        AttachPopupView attachPopupView = new XPopup.Builder(this)
+                .setPopupCallback(new XPopupCallback() {
+                    @Override
+                    public void onCreated(BasePopupView popupView) {
+
+                    }
+
+                    @Override
+                    public void beforeShow(BasePopupView popupView) {
+
+                    }
+
+                    @Override
+                    public void onShow(BasePopupView popupView) {
+
+                    }
+
+                    @Override
+                    public void onDismiss(BasePopupView popupView) {
+
+                    }
+
+                    @Override
+                    public void beforeDismiss(BasePopupView popupView) {
+
+                    }
+
+                    @Override
+                    public boolean onBackPressed(BasePopupView popupView) {
+                        return false;
+                    }
+
+                    @Override
+                    public void onKeyBoardStateChanged(BasePopupView popupView, int height) {
+
+                    }
+
+                    @Override
+                    public void onDrag(BasePopupView popupView, int value, float percent, boolean upOrLeft) {
+
+                    }
+                })
+                .hasShadowBg(false)
+                .isCenterHorizontal(true)
+                .popupPosition(PopupPosition.Bottom)
+                .popupType(PopupType.Bottom)
+                .autoDismiss(true)
+//                            .isDestroyOnDismiss(true) //对于只使用一次的弹窗，推荐设置这个
+//                        .isDarkTheme(true)
+//                        .popupAnimation(PopupAnimation.ScaleAlphaFromCenter) //NoAnimation表示禁用动画
+//                        .isCenterHorizontal(true) //是否与目标水平居中对齐
+//                        .offsetY(-60)
+//                        .offsetX(80)
+//                        .popupPosition(PopupPosition.Top) //手动指定弹窗的位置
+//                        .popupWidth(500)
+                .atView(ll_view)  // 依附于所点击的View，内部会自动判断在上方或者下方显示
+                .asAttachList(new String[]{"分享", "编辑", "不带icon", "分享"},
+                        new int[]{R.mipmap.ic_launcher_round, R.mipmap.ic_launcher_round},
+                        new OnSelectListener() {
+                            @Override
+                            public void onSelect(int position, String text) {
+                                Log.e("ssss", "click " + text);
+                            }
+                        }, R.layout.ll_recyclerview, R.layout.family_bean_item_layout);
+
+        ll_view.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                attachPopupView.show();
             }
         });
     }
--- Spinner/src/main/java/com/example/spinner/SpinnerAdapter.java
+++ Spinner/src/main/java/com/example/spinner/SpinnerAdapter.java
@@ -1,30 +1,57 @@
 package com.example.spinner;
+
 import com.bumptech.glide.Glide;
 import com.bumptech.glide.load.resource.bitmap.CircleCrop;
 import com.bumptech.glide.request.RequestOptions;
 
 import android.content.Context;
-import android.text.TextUtils;
+import android.graphics.Color;
+import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.BaseAdapter;
 import android.widget.ImageView;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 
 public class SpinnerAdapter extends BaseAdapter {
 
     private java.util.List<SpinnerBean> spinnerBeans;
 
     private Context context;
+    private int choosePosition;
+    private Spinner spinner;
 
-    public SpinnerAdapter(Context context, java.util.List<SpinnerBean> familyMemberBeans) {
+    public interface SelectListener {
+        public void clickItem(int position);
+    }
+
+    private SelectListener selectListener = new SelectListener() {
+        @Override
+        public void clickItem(int position) {
+
+        }
+    };
+
+
+    public SpinnerAdapter(Context context, Spinner spinner, java.util.List<SpinnerBean> familyMemberBeans) {
         this.context = context;
         this.spinnerBeans = familyMemberBeans;
+        this.spinner = spinner;
     }
 
     public void update(java.util.List<SpinnerBean> list) {
 //        this.familyMemberBeans.clear();
 //        this.familyMemberBeans.addAll(list);
         this.spinnerBeans = list;
+    }
+
+    public void select(int position) {
+        choosePosition = position;
     }
 
     @Override
@@ -39,22 +66,69 @@ public class SpinnerAdapter extends BaseAdapter {
 
     @Override
     public long getItemId(int position) {
+        Log.e("sss", "getItemId position " + position);
         return position;
     }
 
     @Override
-    public android.view.View getView(int position, android.view.View convertView, ViewGroup parent) {
+    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
         if (null == convertView) {
             LayoutInflater mLayoutInflater = LayoutInflater.from(context);
             convertView = mLayoutInflater.inflate(R.layout.family_bean_item_layout, null);
         }
 
-        ImageView ivFamilyMemberIcon = convertView.findViewById(R.id.iv_family_bean_icon);
+        TextView textView = convertView.findViewById(R.id.tv_text);
+        if (position == spinner.getSelectedItemPosition()) {
+            textView.setTextColor(Color.GREEN);
+        } else {
+            textView.setTextColor(Color.RED);
+        }
+        return convertView;
+    }
+
+
+    //    @Override
+//    public View getDropDownView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+//        Log.e("sss", "getDropDownView position " + position);
+//        return initView(convertView, position, 1);
+//    }
+
+    @Override
+    public android.view.View getView(int position, android.view.View convertView, ViewGroup parent) {
+        Log.e("sss", "getView position " + position + "  " + spinner.getSelectedItemPosition());
+        return initView(convertView, position, 0);
+    }
+
+    private View initView(View convertView, int position, int choose) {
+        if (null == convertView) {
+            LayoutInflater mLayoutInflater = LayoutInflater.from(context);
+            convertView = mLayoutInflater.inflate(R.layout.family_bean_item_layout, null);
+        }
+
+        convertView.setMinimumHeight(87);
+
+        ImageView ivFamilyMemberIcon = convertView.findViewById(R.id.iv_image);
         Glide.with(context).load(spinnerBeans.get(position).getUrl())
                 .apply(RequestOptions.bitmapTransform(new CircleCrop()))
                 .error(R.mipmap.img_header_man)
                 .into(ivFamilyMemberIcon);
-        android.widget.TextView tvFamilyMemberName = convertView.findViewById(R.id.tv_family_member_name);
+        android.widget.TextView tvFamilyMemberName = convertView.findViewById(R.id.tv_text);
+        if (choose == 0) {
+            convertView.setBackgroundColor(Color.TRANSPARENT);
+            convertView.setPadding(5, 5, 45, 5);
+            tvFamilyMemberName.setTextColor(Color.WHITE);
+        }
+
+        if (choose == 1 && position == spinner.getSelectedItemPosition()) {
+            convertView.setBackground(context.getDrawable(R.drawable.spinner_popup_item_background));
+        } else if (choose == 1 && position == 0) {
+            convertView.setBackground(context.getDrawable(R.drawable.spinner_popup_item_background0));
+        }
+
+        if (choose == 1 && spinner.getSelectedItemPosition() == position) {
+            tvFamilyMemberName.setTextColor(Color.parseColor("#28C3F2"));
+        }
+
         tvFamilyMemberName.setText(spinnerBeans.get(position).getName());
         return convertView;
     }
