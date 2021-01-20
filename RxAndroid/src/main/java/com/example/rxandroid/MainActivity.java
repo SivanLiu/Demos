a69a0af1ff16c7c353a6d27f60e32eef2a6335b1
sivanliu <lygforbs0@gmail.com>
2021-01-20 22:48:34 +0800

    add rxjava

--- RxAndroid/src/main/java/com/example/rxandroid/MainActivity.java
+++ RxAndroid/src/main/java/com/example/rxandroid/MainActivity.java
@@ -1,22 +1,25 @@
 package com.example.rxandroid;
 
-import com.jakewharton.rxbinding4.view.RxView;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.jakewharton.rxbinding4.view.RxView;
+
 import java.util.concurrent.TimeUnit;
 
-import androidx.appcompat.app.AppCompatActivity;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.Disposable;
+import io.reactivex.rxjava3.functions.Consumer;
 import kotlin.Unit;
 
 public class MainActivity extends AppCompatActivity {
@@ -36,6 +39,41 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View view) {
                 Log.e(TAG, "onClick: ");
+            }
+        });
+
+
+        Log.e("sss", "single start");
+//        Single.just("sss").delay(5, TimeUnit.SECONDS).subscribe(new Consumer<String>() {
+//            @Override
+//            public void accept(String s) throws Throwable {
+//                Log.e("sss", "single end " + s);
+//            }
+//        });
+
+        final Disposable[] disposable = {null};
+
+        Observable.intervalRange(10, 5, 3, 2, TimeUnit.SECONDS).subscribe(new Observer<Long>() {
+            @Override
+            public void onSubscribe(@NonNull Disposable d) {
+                Log.e("sss", "onSubscribe start");
+                disposable[0] = d;
+            }
+
+            @Override
+            public void onNext(@NonNull Long aLong) {
+                Log.e("sss", "onNext " + aLong);
+            }
+
+            @Override
+            public void onError(@NonNull Throwable e) {
+                Log.e("sss", "onError");
+            }
+
+            @Override
+            public void onComplete() {
+                Log.e("sss", "onComplete");
+                disposable[0].dispose();
             }
         });
 
