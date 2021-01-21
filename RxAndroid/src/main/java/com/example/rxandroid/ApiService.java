be394c9f14a70545d895f27bf06b94d2fc90bd9c
sivanliu <lygforbs0@gmail.com>
2021-01-21 23:09:47 +0800

    add rxjava

--- RxAndroid/src/main/java/com/example/rxandroid/ApiService.java
+++ RxAndroid/src/main/java/com/example/rxandroid/ApiService.java
@@ -3,10 +3,14 @@ package com.example.rxandroid;
 import java.util.List;
 
 import io.reactivex.rxjava3.core.Single;
+import io.reactivex.rxjava3.internal.operators.single.SingleObserveOn;
 import retrofit2.http.GET;
 import retrofit2.http.Query;
 
 public interface ApiService {
     @GET("getJoke/")
     Single<ResultBean<List<JokenBean>>> getJoke(@Query("page") int page, @Query("count") int count, @Query("type") String type);
+
+    @GET("api/qq.php/")
+    Single<Object> getQQ(@Query("qq")int qq);
 }
--- RxAndroid/src/main/java/com/example/rxandroid/MainActivity.java
+++ RxAndroid/src/main/java/com/example/rxandroid/MainActivity.java
@@ -1,27 +1,24 @@
 package com.example.rxandroid;
 
-import com.jakewharton.rxbinding4.view.RxView;
-
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.List;
+
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import io.reactivex.rxjava3.android.schedulers.AndroidSchedulers;
 import io.reactivex.rxjava3.annotations.NonNull;
 import io.reactivex.rxjava3.core.Observable;
 import io.reactivex.rxjava3.core.Observer;
+import io.reactivex.rxjava3.core.Single;
 import io.reactivex.rxjava3.disposables.Disposable;
-import io.reactivex.rxjava3.functions.Consumer;
 import io.reactivex.rxjava3.schedulers.Schedulers;
-import kotlin.Unit;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava3.RxJava3CallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
@@ -54,78 +51,147 @@ public class MainActivity extends AppCompatActivity {
 //                Log.e("sss", "single end " + s);
 //            }
 //        });
+//        final Disposable[] disposable = {null};
+//
+//        Observable.intervalRange(10, 5, 3, 2, TimeUnit.SECONDS).subscribe(new Observer<Long>() {
+//            @Override
+//            public void onSubscribe(@NonNull Disposable d) {
+//                Log.e("sss", "onSubscribe start");
+//                disposable[0] = d;
+//            }
+//
+//            @Override
+//            public void onNext(@NonNull Long aLong) {
+//                Log.e("sss", "onNext " + aLong);
+//            }
+//
+//            @Override
+//            public void onError(@NonNull Throwable e) {
+//                Log.e("sss", "onError");
+//            }
+//
+//            @Override
+//            public void onComplete() {
+//                Log.e("sss", "onComplete");
+//                disposable[0].dispose();
+//            }
+//        });
+//
+//
+//        RxView.clicks(btLogin).throttleFirst(5, TimeUnit.SECONDS)
+//                .subscribe(new Observer<Unit>() {
+//                    @Override
+//                    public void onSubscribe(@NonNull Disposable d) {
+//                        Log.e(TAG, "onSubscribe: ");
+//                    }
+//
+//                    @Override
+//                    public void onNext(@NonNull Unit unit) {
+//                        Log.e(TAG, "onNext: ");
+//                    }
+//
+//                    @Override
+//                    public void onError(@NonNull Throwable e) {
+//                        Log.e(TAG, "onError: ");
+//                    }
+//
+//                    @Override
+//                    public void onComplete() {
+//                        Log.e(TAG, "onComplete: ");
+//                    }
+//                });
 
-        Retrofit retrofit = new Retrofit.Builder()
-                .baseUrl("https://api.apiopen.top")
-                .addConverterFactory(GsonConverterFactory.create())
-                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
-                .build();
-
-        retrofit.create(ApiService.class).getJoke(1, 2, "video")
+        Observable.merge(getJoke().toObservable(), getQQ().toObservable(), getJoke().toObservable())
                 .subscribeOn(Schedulers.io())
                 .observeOn(AndroidSchedulers.mainThread())
-                .subscribe(new Consumer<ResultBean<List<JokenBean>>>() {
-                    @Override
-                    public void accept(ResultBean<List<JokenBean>> objectResultBean) throws Throwable {
-                        Log.e("ssss", objectResultBean.toString());
-                    }
-                }, new Consumer<Throwable>() {
-                    @Override
-                    public void accept(Throwable throwable) throws Throwable {
-                        Log.e("ssss", throwable.toString());
-                    }
-                });
-
-
-        final Disposable[] disposable = {null};
-
-        Observable.intervalRange(10, 5, 3, 2, TimeUnit.SECONDS).subscribe(new Observer<Long>() {
-            @Override
-            public void onSubscribe(@NonNull Disposable d) {
-                Log.e("sss", "onSubscribe start");
-                disposable[0] = d;
-            }
-
-            @Override
-            public void onNext(@NonNull Long aLong) {
-                Log.e("sss", "onNext " + aLong);
-            }
-
-            @Override
-            public void onError(@NonNull Throwable e) {
-                Log.e("sss", "onError");
-            }
-
-            @Override
-            public void onComplete() {
-                Log.e("sss", "onComplete");
-                disposable[0].dispose();
-            }
-        });
-
-
-        RxView.clicks(btLogin).throttleFirst(5, TimeUnit.SECONDS)
-                .subscribe(new Observer<Unit>() {
+                .subscribe(new Observer<Object>() {
                     @Override
                     public void onSubscribe(@NonNull Disposable d) {
-                        Log.e(TAG, "onSubscribe: ");
+                        Log.e("sss onSubscribe", d.toString());
                     }
 
                     @Override
-                    public void onNext(@NonNull Unit unit) {
-                        Log.e(TAG, "onNext: ");
+                    public void onNext(@NonNull Object o) {
+                        Log.e("sss onNext", o.toString());
                     }
 
                     @Override
                     public void onError(@NonNull Throwable e) {
-                        Log.e(TAG, "onError: ");
+                        Log.e("sss onError", e.toString());
                     }
 
                     @Override
                     public void onComplete() {
-                        Log.e(TAG, "onComplete: ");
+                        Log.e("sss onComplete", "onComplete");
                     }
                 });
+//                .subscribe(new Consumer<Object>() {
+//                    @Override
+//                    public void accept(Object o) throws Throwable {
+//                        Log.e("sss accept", o.toString());
+//                    }
+//                }, new Consumer<Throwable>() {
+//                    @Override
+//                    public void accept(Throwable throwable) throws Throwable {
+//                        Log.e("sss throwable", throwable.toString());
+//                    }
+//                }, new Action() {
+//                    @Override
+//                    public void run() throws Throwable {
+//                        Log.e("sss", "onComplete run...");
+//                    }
+//                });
+    }
+
+    private Single<ResultBean<List<JokenBean>>> getJoke() {
+
+        Retrofit retrofit = new Retrofit.Builder()
+                .baseUrl("https://api.apiopen.top")
+//                .baseUrl("https://api.oioweb.cn")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+
+        Single<ResultBean<List<JokenBean>>> joke = retrofit.create(ApiService.class).getJoke(1, 2, "video");
+        return joke;
+        //                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Consumer<ResultBean<List<JokenBean>>>() {
+//                    @Override
+//                    public void accept(ResultBean<List<JokenBean>> objectResultBean) throws Throwable {
+//                        Log.e("ssss", objectResultBean.toString());
+//                    }
+//                }, new Consumer<Throwable>() {
+//                    @Override
+//                    public void accept(Throwable throwable) throws Throwable {
+//                        Log.e("ssss", throwable.toString());
+//                    }
+//                });
+    }
+
+    private Single<Object> getQQ() {
+        Retrofit retrofit2 = new Retrofit.Builder()
+                .baseUrl("https://api.oioweb.cn")
+                .addConverterFactory(GsonConverterFactory.create())
+                .addCallAdapterFactory(RxJava3CallAdapterFactory.create())
+                .build();
+
+        return retrofit2.create(ApiService.class).getQQ(468088615);
+
+//                .subscribeOn(Schedulers.io())
+//                .observeOn(AndroidSchedulers.mainThread())
+//                .subscribe(new Consumer<Object>() {
+//                    @Override
+//                    public void accept(Object objectResultBean) throws Throwable {
+//                        Log.e("sss", "getQQ: "+objectResultBean.toString());
+//                    }
+//                }, new Consumer<Throwable>() {
+//                    @Override
+//                    public void accept(Throwable throwable) throws Throwable {
+//                        Log.e("sss", "throwable: "+throwable.toString());
+//
+//                    }
+//                });
     }
 
 
