d327110988c0f8b966d82b5a843b2e7818b5b978
sivanliu <lygforbs0@gmail.com>
2021-06-13 20:01:55 +0800

    code review

--- vlcdemo/src/main/java/com/example/vlcdemo/MainActivity.kt
+++ vlcdemo/src/main/java/com/example/vlcdemo/MainActivity.kt
@@ -2,10 +2,14 @@ package com.example.vlcdemo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.vlc.lib.VlcPlayer
+import com.vlc.lib.VlcVideoView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        VlcPlayer
     }
 }
